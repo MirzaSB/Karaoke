8ed14f48966b8a3bb1fd93c7f3cee5028f5fe39e
mirzasb <mirzasbaig@nyu.edu>
2017-02-24 20:40:34 -0500

    Latest project code

--- app/controllers/mongo_controller.py
+++ app/controllers/mongo_controller.py
@@ -58,17 +58,23 @@ class MongoController(MongoClient):
             print("Could not connect to MongoDB. The exception message is below:")
             print(e)
 
-    def get_song(self, id):
+    def get_song(self, title, artist):
         """
         Returns a song from MongoDB.
 
-        :param id: str: The "_id" value of the song.
+        :param title: str: The "title" value of the song.
+        :param artist: str: The "artist" value of the song.
         :return: Song: The transformed song object from MongoDB.
         """
-        # Get the song from MongoDB using the "_id" value.
-        songJson = self.songsCollection.find_one({self.CONST_PROPERTY_ID: ObjectId(id)})
+        # Get the song from MongoDB.
+        songJson = self.songsCollection.find_one({self.CONST_PROPERTY_TITLE: title,
+                                                  self.CONST_PROPERTY_ARTIST: artist})
+        if songJson == None:
+            print("No song found.")
+            return
         # Transform the returned dictionary object to a song, and return it.
-        return self.transform_doc_to_song(songJson)
+        transformedSong = self.transform_doc_to_song(songJson)
+        return transformedSong
 
     def insert_song(self, title, artist, youtube):
         """
@@ -98,24 +104,26 @@ class MongoController(MongoClient):
         :return: str: The "_id" value of the inserted song. If the song could not be entered, then "None" is returned.
         """
         if(isinstance(songObj, Song)):
-            return self.insert_song(songObj.get_artist(), songObj.get_title(), songObj.get_link())
+            return self.insert_song(songObj.get_title(), songObj.get_artist(), songObj.get_link())
         else:
             return None
 
-    def remove_song(self, id):
+    def remove_song(self, title, artist):
         """
         Removes a song from the "songs" collection.
 
-        :param id: The "_id" value of the song that is to be removed.
+        :param title: str: The "title" value of the song.
+        :param artist: str: The "artist" value of the song.
         :return: DeletedResult: The object containing all the deleted document data.
         """
         # Remove the document from MongoDB, and get the "deleted_count" value.
-        deletedResult = self.songsCollection.delete_one({self.CONST_PROPERTY_ID: ObjectId(id)})
+        deletedResult = self.songsCollection.delete_one({self.CONST_PROPERTY_TITLE: title,
+                                                         self.CONST_PROPERTY_ARTIST: artist})
         deletedCount = deletedResult.deleted_count
         if(deletedCount == 0):
-            print("The document of the '_id' value, '%s' is not in MongoDB and hence could not be deleted." % id)
+            print("The song of the 'title' value, '%s' is not in MongoDB and hence could not be deleted." % title)
         else:
-            print("The document of the '_id' value, '%s' was found in MongoDB and successfully deleted." % id)
+            print("The song of the '_id' value, '%s' was found in MongoDB and successfully deleted." % title)
         # Return the DeletedResult object.
         return deletedResult
 
@@ -126,7 +134,12 @@ class MongoController(MongoClient):
         :param json: dict: The dictionary object from MongoDB.
         :return: Song: The processed Song object.
         """
-        return Song(json.get(self.CONST_PROPERTY_TITLE, self.CONST_PROPERTY_ARTIST,
-                             self.CONST_PROPERTY_YOUTUBE))
+        title = json.get(self.CONST_PROPERTY_TITLE)
+        artist = json.get(self.CONST_PROPERTY_ARTIST)
+        youtube = json.get(self.CONST_PROPERTY_YOUTUBE)
+        return Song(title, artist, youtube)
 
 mongo = MongoController("localhost", "27017", "admin3", "pa55word")
+mongo.insert_song_object(Song("Safe and Sound", "Capital Cites", "www.youtube.com/watch?v=47dtFZ8CFo8"))
+mongo.insert_song_object(Song("In The End", "Linkin Park", "www.youtube.com/watch?v=eVTXPUF4Oz4"))
+mongo.insert_song_object(Song("Beautiful Now", "Zedd", "www.youtube.com/watch?v=n1a7o44WxNo"))
